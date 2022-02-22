5a52e39d8ab394649294a6797765cecb26458f9f
MaedaKouta <maedakoutaa@gmail.com>
2022-02-22 10:29:08 +0900

    提出用のコミット

--- KotaKadai10/ViewController.swift
+++ KotaKadai10/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    private let prefectures = ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"]
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return prefectures.count
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var identifier = ""
+
+        if indexPath.row % 3 == 0 {
+            identifier = "RedCell"
+        } else if indexPath.row % 3 == 1 {
+            identifier = "GreenCell"
+        } else {
+            identifier = "BlueCell"
+        }
+
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: identifier) else {
+            fatalError("cellの作成に失敗")
+        }
+
+        if let prefectureLabel = cell.viewWithTag(1) as? UILabel, let countLabel = cell.viewWithTag(2) as? UILabel {
+            prefectureLabel.text = prefectures[indexPath.row]
+            countLabel.text = "\(indexPath.row + 1)番目の都道府県です"
+        }
+
+        return cell
+    }
 
 }
-
